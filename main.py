f6c6150af3464cd93f50ba7c77131711db356843
MarcinWypior <marcinwypior1990@gmail.com>
2023-07-02 11:45:56 +0200

    Sending email with quote

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-import smtplib
-import datetime as dt
+import pandas
+from datetime import datetime
 import random
+import smtplib
 
-now = dt.datetime.now()
 my_email = "marcin1java@gmail.com"
 password = "efvrkyjskdqfkcnq"
 
+today = datetime.now()
+today_tuple = (today.day ,today.month )
+#print(today_tuple)
+
+data = pandas.read_csv("birthdays.csv")
+birthday_dict = {(data_row.day,data_row.month): data_row  for (index,data_row) in data.iterrows()}
 
 
-if now.weekday() == 6:
-    with open("quotes.txt", "r") as quotes:
-        list_of_quotes = quotes.readlines()
-        quote_of_day = random.choice(list_of_quotes)
-        print(quote_of_day)
+#print(birthday_dict)
+
+if today_tuple in birthday_dict:
+    birthday_person = birthday_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]",birthday_person["name"])
+        print(contents)
 
     with smtplib.SMTP_SSL("smtp.gmail.com") as connection:
         connection.ehlo()
         connection.login(user=my_email, password=password)
-        connection.sendmail(from_addr=my_email, to_addrs="marcin1java@op.pl", msg=f"Subject:Monday Motivation \n\n {quote_of_day}")
+        connection.sendmail(from_addr=my_email,
+                            to_addrs="marcin1java@op.pl",
+                            msg=f"Subject:Happy Birthsday \n\n {contents}")
         connection.close()
-
